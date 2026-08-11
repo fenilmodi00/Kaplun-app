b2418d4e9624bb4bdeabd1b7c65fa8f939340c5c
fenilmodi00 <fenilmodi00@gmail.com>
2026-08-11 22:17:17 +0530

    chore(repo): delete dead Python/SQLite leftovers and dead proxy env var

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,6 @@
 // Set required env vars before any module imports
 process.env.EXPO_PUBLIC_IG_API_BASE_URL = 'http://localhost:8000';
 process.env.EXPO_PUBLIC_IG_APP_ID = 'test_app_id';
-process.env.EXPO_PUBLIC_IG_API_PROXY_URL = 'https://test-proxy.example.com';
 process.env.EXPO_PUBLIC_IG_OAUTH_REDIRECT_URI = 'https://test-callback.example.com/';
 process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT = 'https://test-appwrite.example.com/v1';
 process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID = 'test-project-id';
